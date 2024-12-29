e867f2adcedc0bbe53d645adaf3657aadda019d7
suxrob-tolqinboyev <tolqinboyevsuxrob053@gmail.com>
2024-12-29 21:23:55 +0500

    add

---
+++ index.js
@@ -0,0 +1,60 @@
+// 1masala
+// const fruits = ["apple", "banana", "cherry"];
+// const [first, second, third] = fruits;
+// const result = `${first}, ${second}, ${third}`;
+// console.log(result);
+
+// 2-masala
+// const talabalar = { Anvar: 18, Jamila: 20, Mirza: 19 };
+// const keys = Object.keys(talabalar);
+// const { length: talabalarSoni } = keys;
+// console.log(talabalarSoni);
+
+// 3-masala
+// const mahsulotlar = { olma: 3000, nok: 4000, banan: 2000 };
+
+// const { olma, nok, banan } = mahsulotlar;
+
+// const sum = olma + nok + banan;
+// console.log(sum);
+
+// masala 4
+// function Odam(ism) {
+//     this.ism = ism
+//   }
+  
+//   Odam.prototype.gapir = function () {
+//     const { ism } = this;
+//     return `Salom, mening ismim ${ism}`;
+//   };
+  
+//   const odam1 = new Odam("Ali");
+//   console.log(odam1.gapir());
+ 
+// 5-masala
+// const matn = "hello";
+
+// const kattaMatn = matn.toUpperCase();
+// console.log(kattaMatn);
+
+// yoki ikkinchi yechim
+// const matn = "hello";
+
+// const [h, e, l1, l2, o] = [...matn];
+
+// const kattaMatn = ${h.toUpperCase()}${e.toUpperCase()}${l1.toUpperCase()}${l2.toUpperCase()}${o.toUpperCase()};
+
+// console.log(kattaMatn);
+
+// masala 6
+// const massiv = [1, 2, 3];
+// const yangiElement = 4;
+
+// const yangiMassiv = [...massiv, yangiElement];
+// console.log(yangiMassiv);
+
+// 7-masala
+// const massivUzunlik = [7, 8, 9];
+// const { length: uzunlik } = massivUzunlik;
+// console.log(uzunlik);
+
